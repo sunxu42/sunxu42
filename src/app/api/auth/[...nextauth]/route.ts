7b2531f6599948f764f195728e03fbf2aa00b16a
sunxu42 <sunxu42@outlook.com>
2025-12-24 16:40:01 +0800

    refactor(auth): use default export for db import in nextauth route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import { db } from "@/lib/db";
+import db from "@/lib/db";
 import bcrypt from "bcrypt";
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
